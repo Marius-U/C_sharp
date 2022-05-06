e17c14196362c7343ef34c8f003fcca6976e697c
Marius Ungurean <umarius94@gmail.com>
2022-05-06 15:53:19 +0300

    Extracting Parent path and file name.

--- AES_GatewayFirmwareEncrypt/AES_GatewayFirmwareEncrypt.cpp
+++ AES_GatewayFirmwareEncrypt/AES_GatewayFirmwareEncrypt.cpp
@@ -4,17 +4,28 @@
 #include <iostream>
 #include <string.h>
 #include "Crypto/myTestHandler.h"
-
+#include <filesystem>
 
 using namespace std;
+using namespace std::filesystem;
+
+
 int main(int argc, char* argv[], char* envp[])
 {
     // Walk through list of strings until a NULL is encountered.
     for (int i = 1; i < argc; i++)
     {
         
-        cout << argv[i] << "\n";
+        cout << argv[i] << "\n\n\n";
     }
+
+    path pathToFirmware(argv[argc - 1]);
+
+    cout << endl;
+    cout << endl;
+    cout << pathToFirmware.parent_path() << endl;
+    cout << pathToFirmware.filename() << endl;
+    cout << endl;
 
     bool flag = true;
     if (flag)
@@ -22,5 +33,6 @@ int main(int argc, char* argv[], char* envp[])
         test();
         flag = false;
     }
-    system("pause");
+
+   // system("pause");
 }
