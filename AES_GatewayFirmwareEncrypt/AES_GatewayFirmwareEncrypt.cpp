c98fe5ecad5ecc13bd1808caa24a9ecf2b973d26
Marius Ungurean <umarius94@gmail.com>
2022-05-07 14:52:54 +0300

    Encrypt/Decrypt Firmware Done!

--- AES_GatewayFirmwareEncrypt/AES_GatewayFirmwareEncrypt.cpp
+++ AES_GatewayFirmwareEncrypt/AES_GatewayFirmwareEncrypt.cpp
@@ -126,3 +126,26 @@ void decrypt(BlockCipher* cipher, uint8_t* encryptedData)
 
     cipher->decryptBlock(encryptBuffer, encryptedData);
 }
+
+/*
+* with this function you can easily load your file into the vector like this:
+* std::vector<BYTE> fileData = readfile("myfile.bin");
+*/
+
+std::vector<BYTE> readFile(const char* filename)
+{
+    // open the file:
+    std::streampos fileSize;
+    std::ifstream file(filename, std::ios::binary);
+
+    // get its size:
+    file.seekg(0, std::ios::end);
+    fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    // read the data:
+    std::vector<BYTE> fileData(fileSize);
+    file.read((char*)&fileData[0], fileSize);
+    return fileData;
+}
+
